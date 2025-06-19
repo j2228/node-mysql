8bab0df561b3c0f454fb592941b9f0c362e82fae
j2228 <j2228@matsue-ct.ac.jp>
2025-06-19 05:37:52 +0000

    chapter13-目次1,2終わり

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password:'password',
+  database:'todo_app'
+});
 //レンダリング 
 //表示用のデータをもとに、
 //内容を整形して表示すること
@@ -15,9 +22,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req,res,next){
+  connection.connect((err)=>{
+    if(err){
+      console.log('error connecting: ' + errr.stack);
+      return
+    }
+    console.log('success');
+  });
+
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
+
+
 
 module.exports = router;
