f836ea53790a614154e25f4b6d96a955c0145ae8
j2228 <j2228@matsue-ct.ac.jp>
2025-06-26 05:36:25 +0000

    Chapter15 signin ページまで　次はnpm install cookie-sessionから

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password:'password',
-  database: 'todo_app'
-});
 
 router.get('/', function (req, res, next) {
   knex("tasks")
@@ -43,4 +35,6 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
 module.exports = router;
