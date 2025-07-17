bbeecfe3da699bd42100ad7a94b02ac46406c2b5
j2228 <j2228@matsue-ct.ac.jp>
2025-07-17 04:43:26 +0000

    Chapter18-end

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,27 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const knex = require("../db/knex");
 const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const cookieSession = require("cookie-session");
+const secret = "secretCuisine123";
 
 module.exports = function (app) {
+  passport.serializeUser(function (user, done) {
+      console.log("serializeUser");
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async function (id, done) {
+      console.log("deserializeUser");
+
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (error) {
+      done(error, null);
+    }
+  });
+
   passport.use(new LocalStrategy({
       usernameField: "username",
       passwordField: "password",
@@ -28,4 +47,16 @@ module.exports = function (app) {
         });
     }
   ));
+
+  app.use(
+    cookieSession({
+      name: "session",
+      keys: [secret],
+
+      // Cookie Options
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    })
+  );
+
+  app.use(passport.session());
 };
--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,37 @@ const knex = require('../db/knex');
 
 
 router.get('/', function (req, res, next) {
-  console.log('ルートにアクセス'); // 追加
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  // console.log(`isAuth: ${isAuth}`);
 
-  //req.session.userid はサインイン時にデータが入るため、未サインインの間はundefinedとなり、Boolean(userId) はfalseを返します。
-  knex("tasks")
-    .select("*")
+  let query = knex("tasks").select("*");
+  if (isAuth) {
+    query = query.where({ user_id: userId });
+  }
+
+  query
     .then(function (results) {
-      console.log(results);
       res.render('index', {
         title: 'ToDo App',
         todos: results,
-        isAuth:isAuth,
+        isAuth: isAuth,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
+        todos: [],
+        isAuth: isAuth,
       });
     });
 });
-
 router.post('/', function (req, res, next) {
   const userId= req.session.userid;
   const isAuth = Boolean(userId);
   const todo = req.body.add;
   knex("tasks")
-    .insert({user_id: 1, content: todo})
+    .insert({user_id: userId, content: todo})
     .then(function () {
       res.redirect('/')
     })
